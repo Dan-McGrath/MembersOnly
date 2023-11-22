4409aa9d60a0d9771c6b700c03ea1305574179d0
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-22 01:34:51 -0500

    add controllers to route

--- members-only/routes/index.js
+++ members-only/routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const index_controller = require("../controllers/indexController");
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Message Board", user: req.user });
-});
+router.get("/", index_controller.index);
+router.post("/", index_controller.index_post);
 
 module.exports = router;
