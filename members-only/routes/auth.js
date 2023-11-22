b9295746abf59583e522df2bda431c14dacef624
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-22 01:34:30 -0500

    add controller to route

--- members-only/routes/auth.js
+++ members-only/routes/auth.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/login", function (req, res, next) {
-  res.render("log-in", {
-    title: "Log in",
-  });
-});
+const auth_controller = require("../controllers/authController");
 
-router.get("/sign_up", function (req, res, next) {
-  res.render("sign-up-form", {
-    title: "Sign Up",
-  });
-});
+router.get("/login", auth_controller.login);
+router.post("/login", auth_controller.login_post);
+
+router.get("/sign_up", auth_controller.sign_up);
+
+router.post("/sign_up", auth_controller.sign_up_post);
 
 module.exports = router;
