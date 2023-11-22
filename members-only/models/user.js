3c1812ac9a77f4ffed10b563cc80da80b9bd5a25
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-22 00:44:26 -0500

    add email and default for membership

--- members-only/models/user.js
+++ members-only/models/user.js
@@ -5,9 +5,15 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   last_name: { type: String, required: true, maxLength: 100 },
+  email: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
-  membership_status: {type: String, required: true, enum: ['Public', 'Private', "Admin"]}
+  membership_status: {
+    type: String,
+    required: true,
+    enum: ["Public", "Private", "Admin"],
+    default: "Public",
+  },
 });
 
 UserSchema.virtual("full_name").get(function () {
