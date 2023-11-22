1bb6dbd97d44c4328915df1cf3e2b10692d7c50a
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-22 15:37:20 -0500

    add user model

--- members-only/controllers/indexController.js
+++ members-only/controllers/indexController.js
@@ -1,6 +1,8 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
+const User = require("../models/user");
+
 exports.index = asyncHandler(async (req, res, next) => {
   res.render("index", { title: "Message Board", user: req.user });
 });
