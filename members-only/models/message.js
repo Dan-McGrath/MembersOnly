fb6db0beb991a8622d663208fae38a7b1aacc049
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-21 21:30:02 -0500

    create schema

--- members-only/models/message.js
+++ members-only/models/message.js
@@ -2,4 +2,15 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const MessageSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true, trim: true },
+  message: { type: String, required: true, maxLength: 250, trim: true },
+  time_stamp: { type: Date, default: Date.now },
+});
+
+MessageSchema.virtual("url").get(function () {
+  return `/messageboard/message/${this._id}`;
+});
+
 module.exports = mongoose.model("Message", MessageSchema);
