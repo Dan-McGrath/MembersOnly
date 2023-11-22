44550773ec588e372403bb1131b8fa2a848938be
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-22 16:02:42 -0500

    add controller to route

--- members-only/routes/messageBoard.js
+++ members-only/routes/messageBoard.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const messageboard_controller = require("../controllers/messageController");
+
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+router.get("/", messageboard_controller.messageboard);
 
 module.exports = router;
