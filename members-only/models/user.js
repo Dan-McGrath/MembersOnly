866ca05c3baa31d4889a6aeebca11a1f5a61142e
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-21 21:30:06 -0500

    create schema

--- members-only/models/user.js
+++ members-only/models/user.js
@@ -2,4 +2,20 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  last_name: { type: String, required: true, maxLength: 100 },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  membership_status: {type: String, required: true, enum: ['Public', 'Private']}
+});
+
+UserSchema.virtual("full_name").get(function () {
+  return `${this.last_name}, ${this.first_name}`;
+});
+
+UserSchema.virtual("url").get(function () {
+  return `/messageboard/user/${this._id}`;
+});
+
 module.exports = mongoose.model("User", UserSchema);
