fd48de7e9644f7bcf7ef1cc3458a13199704e1da
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-22 22:27:32 -0500

    add different routes depending on membership status

--- members-only/routes/membership.js
+++ members-only/routes/membership.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const membership_controller = require("../controllers/membershipController");
 
-router.get("/", membership_controller.change_membership_get);
-router.get("/", membership_controller.change_membership_post);
+router.get("/private", membership_controller.change_membership_private_get);
+router.post("/private", membership_controller.change_membership_private_post);
+router.get("/admin", membership_controller.change_membership_admin_get);
+router.post("/admin", membership_controller.change_membership_admin_post);
 
 module.exports = router;
