a69316a0fc13f08d25017f9fac52b653ed18a505
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-22 19:04:27 -0500

    update route

--- members-only/routes/auth.js
+++ members-only/routes/auth.js
@@ -7,7 +7,8 @@ router.get("/login", auth_controller.login);
 router.post("/login", auth_controller.login_post);
 
 router.get("/sign_up", auth_controller.sign_up);
-
 router.post("/sign_up", auth_controller.sign_up_post);
+
+router.get("/log_out", auth_controller.log_out);
 
 module.exports = router;
