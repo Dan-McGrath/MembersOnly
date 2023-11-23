97add35fac5120cb4fd2c25bf031bbafb5018c39
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-22 19:04:42 -0500

    add post controller to post route

--- members-only/routes/messageBoard.js
+++ members-only/routes/messageBoard.js
@@ -5,5 +5,6 @@ const messageboard_controller = require("../controllers/messageController");
 
 /* GET users listing. */
 router.get("/", messageboard_controller.messageboard);
+router.post('/', messageboard_controller.messageboard_post)
 
 module.exports = router;
