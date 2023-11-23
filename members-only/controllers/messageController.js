a1a213b9dc2f6e5d553fb58972a58cbbf32af9f8
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-22 19:04:05 -0500

    create controller to post a message

--- members-only/controllers/messageController.js
+++ members-only/controllers/messageController.js
@@ -13,3 +13,35 @@ exports.messageboard = asyncHandler(async (req, res, next) => {
     messages: messages,
   });
 });
+
+exports.messageboard_post = [
+  body("title", "Title must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("message", "Message must not be empty")
+    .trim()
+    .isLength({ min: 1, max: 250 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const messages = await Message.find().exec();
+
+    const message = new Message({
+      user: req.user,
+      title: req.body.title,
+      message: req.body.message,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("messageboard", {
+        title: "Message Board",
+        messages: messages,
+        newMessage: message,
+        errors: errors.array(),
+      });
+    } else {
+      await message.save();
+      res.redirect("/messageboard");
+    }
+  }),
+];
