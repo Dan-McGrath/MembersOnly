4ad9ec8ae9141d4c1da8207907bab99893de3cbe
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-22 19:03:50 -0500

    add bycrypt and create controllers to create a user and logout

--- members-only/controllers/authController.js
+++ members-only/controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const bycrypt = require("bcryptjs");
+const passport = require("passport");
 
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
@@ -9,10 +11,9 @@ exports.login = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.login_post = asyncHandler(async (req, res, next) => {
-  res.render("log-in", {
-    title: "Log in",
-  });
+exports.login_post = passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
 });
 
 exports.sign_up = asyncHandler(async (req, res, next) => {
@@ -21,4 +22,78 @@ exports.sign_up = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.sign_up_post = asyncHandler(async (req, res, next) => {});
+exports.sign_up_post = [
+  body("first_name", "First name must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("last_name", "Last name must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("email", "Email must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .custom(async (value) => {
+      const user = await User.findOne({ email: value });
+      if (user) {
+        throw new Error("E-mail already in use");
+      }
+    }),
+  body("username", "Username must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .custom(async (value) => {
+      const user = await User.findOne({ username: value });
+      if (user) {
+        throw new Error("Username already in use");
+      }
+    }),
+  body("password", "Password must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("confirm-password").custom((value, { req }) => {
+    if (value === req.body.password) {
+      return true;
+    } else {
+      throw new Error("Passwords do not match");
+    }
+  }),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const hash = await bycrypt.hash(req.body.password, 10);
+
+    const user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      username: req.body.username,
+      password: hash,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("sign-up-form", {
+        title: "Sign Up",
+        user: user,
+        errors: errors.array(),
+      });
+    } else {
+      await user.save();
+      res.redirect("/login");
+    }
+  }),
+];
+
+exports.log_out = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+};
